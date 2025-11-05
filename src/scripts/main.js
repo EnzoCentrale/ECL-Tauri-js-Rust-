ffdb4c97b291ccaec23019023841bc6ea5384cf5
Ducros Enzo <enzo.ducros@etu.ec-lyon.fr>
2025-11-05 16:28:13 +0100

    Css and html good now just the icons left

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,28 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/ 
 var Macro_run = false
 
+
 async function Call_rust() {
   var user = String(document.forms["Credentials"]["username"].value);
   var pass = String(document.forms["Credentials"]["password"].value);
-  console.log("this is working 1");
   await window.__TAURI__.core.invoke("listen_for_cmd_r_and_write", { user: user , pass : pass });
-  alert("Macro is on , Do CMD+R when on ECL+ Page")
-  console.log("this is working 2");
+  document.getElementById("red-dot").style.backgroundColor="#4f8f4aff";
+  console.log("Rust Function called without problem");
 
 
   //IMMA CLEAr this form
   document.forms["Credentials"]["username"].value = "";
-  document.forms["Credentials"]["username"].value = "";
+  document.forms["Credentials"]["password"].value = "";
 
 };
 
 async function Stop_rust() {
   await window.__TAURI__.core.invoke("stop_listening")
+  document.getElementById("red-dot").style.backgroundColor="#b12121ff"
   console.log("rust was stopped")
 }
+
+  
 
 
 window.Call_rust = Call_rust;
