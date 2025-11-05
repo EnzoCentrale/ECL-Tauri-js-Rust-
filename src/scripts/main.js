4bb8104d3d4050e54b73243d676cf027277da378
Ducros Enzo <enzo.ducros@etu.ec-lyon.fr>
2025-11-05 10:25:29 +0100

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+
+import { invoke } from '@tauri-apps/api/core';
+
+
+async function Call_rust() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  var user = String(document.forms["Credentials"]["username"].value);
+  var pass = String(document.forms["Credentials"]["password"].value);
+  alert("this is working 1");
+  await invoke("listen_for_cmd_r_and_write", { name: user , pass : pass });
+  alert("this is working 2");
+};
+
+function Test(){
+  alert("yess")
+}
+
+/*window.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelector("Credentials").addEventListener("submit", (e) => {
+    e.preventDefault();
+    Call_rust();
+  });
+});*/
+
+
